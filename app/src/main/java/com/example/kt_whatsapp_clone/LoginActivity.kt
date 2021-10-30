26e3c0fea7b7b6396b08958022d0c5669b3cebba
Abhishek <absk.sharma7@gmail.com>
2021-10-30 22:56:02 +0530

    Added Login Activity

---
+++ app/src/main/java/com/example/kt_whatsapp_clone/LoginActivity.kt
@@ -0,0 +1,61 @@
+package com.example.kt_whatsapp_clone
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
+import com.example.kt_whatsapp_clone.databinding.ActivityLoginBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var phoneNumber: String
+    private lateinit var countryCode: String
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var nextBtn: Button = binding.NextBtn
+
+        binding.etPhoneNumber.addTextChangedListener {
+            nextBtn.isEnabled = !(it.isNullOrEmpty() || it.length < 10)
+        }
+
+        nextBtn.setOnClickListener {
+            checkNumber()
+        }
+    }
+
+    private fun checkNumber() {
+        countryCode = binding.ccp.selectedCountryCodeWithPlus
+        phoneNumber = countryCode + binding.etPhoneNumber.text.toString()
+        notifyUser()
+    }
+
+    private fun notifyUser() {
+        MaterialAlertDialogBuilder(this).apply {
+            setMessage(
+                "We will be verifying the Phone number:$phoneNumber\n" +
+                        "Is this Ok,or would you like to edit this number?"
+            )
+            setPositiveButton("OK") { _, _ ->
+                showOtpActivity()
+            }
+            setNegativeButton("EDIT") { dialog, which ->
+                dialog.dismiss()
+            }
+            setCancelable(false)
+            create()
+            show()
+        }
+    }
+
+    private fun showOtpActivity() {
+        startActivity(Intent(this,OtpActivity::class.java))
+        finish()
+    }
+}
